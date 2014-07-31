3199cbe6daaa4c401b8294276eb78df41c532332
Cameron <cameron.p.buckingham@gmail.com>
2014-07-31 11:26:58 -0600

    as far as I could do

--- lib/gschool_enumerable.rb
+++ lib/gschool_enumerable.rb
@@ -1,3 +1,36 @@
 module GschoolEnumerable
 
+  def g_each
+    for item in self
+      yield item
+    end
+    return self
+  end
+
+  def g_select
+    arr = []
+    self.g_each do |item|
+      arr << item if yield (item)
+    end
+    arr
+  end
+
+  def g_reject(&block)
+    arr= []
+    self.g_select do {|item| block}
+      arr
+    end
+
+    def g_map
+      arry=[]
+      self.g_each do {|yield| [yield,-yield]}
+      arry
+        end
+
+    end
+
+    def g_find
+      
+    end
+  end
 end
